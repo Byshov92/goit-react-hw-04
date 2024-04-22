822b5bc366c8bfddf4f6c5a7d7dc972474f23b8c
Byshov92 <dimabyshov@gmail.com>
2024-04-22 19:10:00 +0300

    folders check

---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import toast, { Toaster } from "react-hot-toast";
+import css from "./SearchBar.module.css";
+
+const SearchBar = ({ onSubmit }) => {
+  const [query, setQuery] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!query.trim()) {
+      toast.error("Please enter a search query");
+      return;
+    }
+    onSubmit(query);
+  };
+
+  return (
+    <header className={css.searchBar}>
+      <form className={css.searchForm} onSubmit={handleSubmit}>
+        <input
+          className={css.input}
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button className={css.btn} type="submit">
+          Search
+        </button>
+      </form>
+      <Toaster />
+    </header>
+  );
+};
+
+export default SearchBar;
